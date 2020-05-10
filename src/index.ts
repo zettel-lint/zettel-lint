d24c70f52965785e206ff631a3ad58cd8dc4b05d
Craig Nicol <Craig.Nicol@screenmedia.co.uk>
2020-05-10 22:21:57 +0000

    Walking CLI skeleton

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,24 @@
 #!/usr/bin/env node
 
 import clear from "clear";
+import chalk from "chalk";
+import figlet from "figlet";
 import path from "path";
-import commander from "commander";
+import program from "commander";
 
 clear();
-console.log("linting...");
+console.log(
+    chalk.red(
+      figlet.textSync('zettel-lint', { horizontalLayout: 'full' })
+    )
+  );
+
+program
+  .version('0.9.0')
+  .description("A linter/compiler for Zettel markdown repositories")
+  .option('-d, --daily', "Create daily entry if it doesn't exist")
+  .parse(process.argv);
+
+if (!process.argv.slice(2).length) {
+   program.outputHelp();
+}
