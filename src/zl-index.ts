a8d00c7be8dc93a319d1e38a3df0ed26c5b287c3
Craig Nicol <craig.nicol@screenmedia.co.uk>
2020-06-29 23:46:49 +0100

    Backlinks

--- src/zl-index.ts
+++ src/zl-index.ts
@@ -116,7 +116,7 @@ class WikiCollector extends RegexCollector
   protected format(references: formatData[]): string {
     var backList : {[target:string]: string[]} = invertDictionary(references);
 
-    return references.map(r => "* [" + r.id + "] = " + r.filename + ":\n  * " + (r.data.length > 0 ? r.data : "No links") + "\n  * " + (backList[r.id ?? ""] ?? "No backlinks")).join("\n");
+    return references.map(r => "* [" + r.id + "] = " + r.filename + ":\n  * " + (r.data.length > 0 ? r.data : "No links") + "\n  * " + (backList["[" + (r.id ?? "") + "]"] ?? "No backlinks")).join("\n");
   }
   readonly dataName = "Links";
   readonly regex = /\[\d{8,14}\]/g;
