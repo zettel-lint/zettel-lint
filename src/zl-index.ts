9a6bc46611806580a477d394e60583395b9c82b0
Craig Nicol <craig.nicol@screenmedia.co.uk>
2020-06-29 23:55:31 +0100

    Links in orphans

--- src/zl-index.ts
+++ src/zl-index.ts
@@ -127,7 +127,7 @@ class OrphanCollector extends RegexCollector
   protected format(references: formatData[]): string {
     return references
         .filter(r => r.data.length > 0 && r.id !== undefined)
-        .map(r => "* " + r.filename + ": " + r.data.join()).join("\n");
+        .map(r => "* [" + r.id + "] `" + r.filename + "`: " + r.data.join()).join("\n");
   }
   readonly dataName = "Orphans";
   readonly regex = /\[[a-zA-Z0-9\[]+[a-zA-Z ]+.*\][^\(]/g;
