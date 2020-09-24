e325a6a22bef8257898d9d559ef545944dae14bd
Craig Nicol <craig.nicol@screenmedia.co.uk>
2020-09-25 00:14:14 +0100

    Bump version number

--- src/zl.ts
+++ src/zl.ts
@@ -8,7 +8,7 @@ import indexerCommand from "./zl-index";
 import importerCommand from "./zl-import";
 
 program
-  .version('0.9.8')
+  .version('0.9.10')
   .description("A linter/compiler for Zettel markdown repositories")
   .command("cron <cron-mode-args>", "Create daily entry if it doesn't exist")
   ;
