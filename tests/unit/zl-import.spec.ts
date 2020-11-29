a7e2d46c959d24d29b342c59dc39fbb152aa5253
craignicol <craig.nicol@gmail.com>
2020-11-29 23:42:55 +0000

    import cli tests

--- tests/unit/zl-import.spec.ts
+++ tests/unit/zl-import.spec.ts
@@ -3,3 +3,11 @@ import importerCommand from "../../src/zl-import";
 test('adds import to cli', () => {
     expect(importerCommand().name()).toBe("import");
   });
+
+test('can parse source', () => {
+  expect(importerCommand().parse(["node", "zl", "--source", "trello", "--path", "."]).source).toBe("trello");
+});
+
+test('can parse path', () => {
+  expect(importerCommand().parse(["node", "zl", "--source", "trello", "--path", "."]).path).toBe(".");
+});
