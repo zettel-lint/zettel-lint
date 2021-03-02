6d7b077c363a39a9bcc6a8d14fbfcaf899534c62
Craig Nicol <craig.nicol@gmail.com>
2021-03-02 12:55:29 +0000

    Version bump to fix auto-tagging

--- src/zl.ts
+++ src/zl.ts
@@ -9,7 +9,7 @@ import importerCommand from "./zl-import";
 import notesCommand from "./zl-notes";
 
 program
-  .version('0.9.14')
+  .version('0.9.15')
   .description("A linter/compiler for Zettel markdown repositories")
   .command("cron <cron-mode-args>", "Create daily entry if it doesn't exist")
   ;
