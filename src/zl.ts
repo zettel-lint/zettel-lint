27de09607c89903f38caf7b6fba4eeef85ced9f1
Craig Nicol <craig.nicol@gmail.com>
2022-10-18 01:11:34 +0100

    Bump version

--- src/zl.ts
+++ src/zl.ts
@@ -10,7 +10,7 @@ import notesCommand from "./zl-notes";
 // THIS import is outside `src/` folder so fails the build
 // import {version as packageVersion} from "../package.json";
 
-export const version = "0.10.20";
+export const version = "0.10.21";
 
 program
   .version(version)
