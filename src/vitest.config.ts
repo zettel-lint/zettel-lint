3eac416ef8a6c7a9288736c4249b3f14af799337
craignicol <craig.nicol@gmail.com>
2025-04-21 00:21:38 +0000

    Fix config and postbuild for ci

--- src/vitest.config.ts
+++ src/vitest.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
+    exclude: ['**/node_modules/**', '**/lib/**'],
     coverage: {
       reporter: ['text', 'json', 'html'],
       all: true,
